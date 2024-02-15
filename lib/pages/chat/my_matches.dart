c3a2c444a38d355d326638878ae3421215f1a90b
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2024-02-15 04:49:44 -0800

    update

--- lib/pages/chat/my_matches.dart
+++ lib/pages/chat/my_matches.dart
@@ -20,9 +20,14 @@ class MyMatches extends StatelessWidget {
           backgroundColor: ColorRes.white,
           leading: Builder(
             builder: (BuildContext context) {
-              return Icon(
-                Icons.arrow_back_ios_rounded,
-                color: ColorRes.appColor,
+              return IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios_rounded,
+                  color: ColorRes.appColor,
+                ),
               );
               Image.asset(
                 'assets/icons/drawer.png',
@@ -40,7 +45,7 @@ class MyMatches extends StatelessWidget {
                 return IconButton(
                   icon: Image.asset(
                     'assets/icons/Filter Icon.png',
-                    scale: 3,
+                    scale: 1.2,
                   ),
                   onPressed: () {
                     Scaffold.of(context).openDrawer();
@@ -67,115 +72,115 @@ class MyMatches extends StatelessWidget {
                 height: 550,
                 child: ListView.builder(
                   itemCount: value.image.length,
-                  itemBuilder: (context, index) => GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ChatScreen(),
-                      ));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 5),
-                      child: Container(
-                          padding: EdgeInsets.all(15),
-                          height: 80,
-                          width: MediaQuery.of(context).size.width,
-                          color: ColorRes.white,
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Image.asset(
-                                    value.image[index],
-                                    scale: 3,
-                                  ),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 15),
-                                        child: Row(
-                                          children: [
-                                            Text('Patrcia',
-                                                style: TextStyle(
-                                                    color: ColorRes.darkGrey,
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.w700)),
-                                            SizedBox(
-                                              width: 130,
-                                            ),
-                                          ],
-                                        ),
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Container(
+                        padding: EdgeInsets.all(15),
+                        height: 80,
+                        width: MediaQuery.of(context).size.width,
+                        color: ColorRes.white,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  value.image[index],
+                                  scale: 3,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: Row(
+                                        children: [
+                                          Text('Patrcia',
+                                              style: TextStyle(
+                                                  color: ColorRes.darkGrey,
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w700)),
+                                          SizedBox(
+                                            width: 130,
+                                          ),
+                                        ],
                                       ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 15),
-                                        child: SizedBox(
-                                          width: 260,
-                                          child: Text(
-                                            overflow: TextOverflow.ellipsis,
-                                            'Fashion Designer',
-                                            style: TextStyle(
-                                                color: Color(0xffACACAC)),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: SizedBox(
+                                        width: 230,
+                                        child: Text(
+                                          overflow: TextOverflow.ellipsis,
+                                          'Fashion Designer',
+                                          style: TextStyle(
+                                            color: Color(0xffACACAC),
                                           ),
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 1),
-                                    child: Container(
-                                      height: 25,
-                                      width: 25,
-                                      decoration: BoxDecoration(
-                                        color: ColorRes.appColor,
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5, vertical: 5),
+                                  child: Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      // image: DecorationImage(image: AssetImage("assets/icons/Chat.png"),scale: 2),
+                                      color: ColorRes.appColor,
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Image.asset(
+                                        "assets/icons/Chat.png",
+                                        color: ColorRes.white,
+                                        height: 16,
                                       ),
-                                      child:Image.asset("assets/icons/Chat.png",height: 16),
                                     ),
                                   ),
-                                ],
-                              ),
-                            ],
-                          )
-                          /* Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(0),
-                            child: Image.asset('assets/icons/Add Image (1).png',scale: 3,),
-                          ),
-                      Row(
-                        children: [
-                          Column(
-                            children: [
-                              Text('Patrcia',style: TextStyle(
-                                  color: ColorRes.darkGrey, fontSize: 15,fontWeight: FontWeight.w700
-                              )),
-                              Text('08:33 PM')
-                            ],
-                          )
-                        ],
-                      )
-                      */ /*    Column(
-                            children: [
-                              Row(
-                                children: [
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                        /* Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(0),
+                          child: Image.asset('assets/icons/Add Image (1).png',scale: 3,),
+                        ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text('Patrcia',style: TextStyle(
+                                color: ColorRes.darkGrey, fontSize: 15,fontWeight: FontWeight.w700
+                            )),
+                            Text('08:33 PM')
+                          ],
+                        )
+                      ],
+                    )
+                    */ /*    Column(
+                          children: [
+                            Row(
+                              children: [
 
-                                  Text('Patrcia',style: TextStyle(
-                                      color: ColorRes.darkGrey, fontSize: 15,fontWeight: FontWeight.w700
-                                  )),
-                                  Text('08:33 PM')
-                                ],
-                              ),
-                            ],
-                          ),*/ /*
-                         ,
-                        ],
-                      ),*/
-                          ),
-                    ),
+                                Text('Patrcia',style: TextStyle(
+                                    color: ColorRes.darkGrey, fontSize: 15,fontWeight: FontWeight.w700
+                                )),
+                                Text('08:33 PM')
+                              ],
+                            ),
+                          ],
+                        ),*/ /*
+                       ,
+                      ],
+                    ),*/
+                        ),
                   ),
                 ),
               )
