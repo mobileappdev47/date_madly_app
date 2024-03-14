6835719028c5716580d79027b849e588a6d5d5bb
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2024-03-14 17:59:38 +0530

    screen sharing restriction remove

--- android/app/src/main/kotlin/com/datemadly/date/find_partner/love/date_madly_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/datemadly/date/find_partner/love/date_madly_app/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.lovecirco.app
 
 import io.flutter.embedding.android.FlutterActivity
-import android.view.WindowManager.LayoutParams
+//import android.view.WindowManager.LayoutParams
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        window.addFlags(LayoutParams.FLAG_SECURE)
+//        window.addFlags(LayoutParams.FLAG_SECURE)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
